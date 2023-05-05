7903c41b5947591444229eb5cd6b0af9c35224c2
Rhianna Dicent <rhiannadicent@gmail.com>
2023-05-05 13:19:27 -0400

    day3

---
+++ mymodule.py
@@ -0,0 +1,28 @@
+
+
+
+def footage(length, width):
+
+    length = int(input("Whats is the length"))
+    width = int(input("What is the width"))
+    area = length * width
+    print(area)
+
+
+footage(5, 7)
+
+import math
+
+def circum(num):
+    circumf = 2* math.pi * num
+    print(circumf)
+
+circum(8)
+
+
+
+
+
+  
+
+
